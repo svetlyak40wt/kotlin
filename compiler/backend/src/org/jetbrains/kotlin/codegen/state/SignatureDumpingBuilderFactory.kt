3326ea39e77065d7a4f6486b194331290fa27188
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-03-12 15:06:07 +0300

    ~ change format, dump class declarations entirely

--- compiler/backend/src/org/jetbrains/kotlin/codegen/state/SignatureDumpingBuilderFactory.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/state/SignatureDumpingBuilderFactory.kt
@@ -21,8 +21,11 @@ import org.jetbrains.kotlin.codegen.ClassBuilder
 import org.jetbrains.kotlin.codegen.ClassBuilderFactory
 import org.jetbrains.kotlin.codegen.DelegatingClassBuilder
 import org.jetbrains.kotlin.codegen.DelegatingClassBuilderFactory
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.renderer.DescriptorRenderer
 import org.jetbrains.kotlin.resolve.jvm.diagnostics.JvmDeclarationOrigin
+import org.jetbrains.kotlin.resolve.jvm.diagnostics.MemberKind
+import org.jetbrains.kotlin.resolve.jvm.diagnostics.RawSignature
 import org.jetbrains.org.objectweb.asm.FieldVisitor
 import org.jetbrains.org.objectweb.asm.MethodVisitor
 import java.io.BufferedWriter
@@ -34,8 +37,13 @@ class SignatureDumpingBuilderFactory(
         val destination: File
 ) : DelegatingClassBuilderFactory(builderFactory) {
 
+    companion object {
+        val MEMBER_RENDERER = DescriptorRenderer.COMPACT_WITH_MODIFIERS
+        val TYPE_RENDERER = DescriptorRenderer.withOptions { }
+    }
 
     private val outputStream: BufferedWriter by lazy {
+        println("INFO: Dumping signatures to $destination")
         destination.bufferedWriter().apply { append("[\n") }
     }
     private var firstClassWritten: Boolean = false
@@ -47,51 +55,52 @@ class SignatureDumpingBuilderFactory(
     }
 
     override fun newClassBuilder(origin: JvmDeclarationOrigin): DelegatingClassBuilder {
-        if (firstClassWritten) outputStream.append(",\n") else firstClassWritten = true
-
         return SignatureDumpingClassBuilder(origin, delegate.newClassBuilder(origin))
     }
-
 
 
     private inner class SignatureDumpingClassBuilder(val origin: JvmDeclarationOrigin, val _delegate: ClassBuilder) : DelegatingClassBuilder() {
         override fun getDelegate() = _delegate
 
-        init {
-            outputStream.append("\t{\n")
-            origin.descriptor?.let {
-                outputStream.append("\t\t").appendNameValue("descriptor", DescriptorRenderer.FQ_NAMES_IN_TYPES.render(it)).append(",\n")
-            }
-
-        }
-
-        private var firstMethodWritten = false
+        private val signatures = mutableListOf<Pair<RawSignature, DeclarationDescriptor?>>()
+        private var javaClassName: String? = null
 
         override fun defineClass(origin: PsiElement?, version: Int, access: Int, name: String, signature: String?, superName: String, interfaces: Array<out String>) {
-            outputStream.append("\t\t").appendNameValue("class", name).append(", \n")
-            outputStream.append("\t\t").appendQuoted("members").append(": [\n")
+            javaClassName = name
+
             super.defineClass(origin, version, access, name, signature, superName, interfaces)
         }
 
         override fun newMethod(origin: JvmDeclarationOrigin, access: Int, name: String, desc: String, signature: String?, exceptions: Array<out String>?): MethodVisitor {
-            if (firstMethodWritten) outputStream.append(",\n") else firstMethodWritten = true
-
-            outputStream.append("\t\t\t{")
-            origin.descriptor?.let {
-                outputStream.appendNameValue("descriptor", DescriptorRenderer.FQ_NAMES_IN_TYPES.render(it)).append(", ")
-            }
-            outputStream.appendNameValue("name", name).append(", ")
-            outputStream.appendNameValue("desc", desc).append("}")
-
+            signatures += RawSignature(name, desc, MemberKind.METHOD) to origin.descriptor
             return super.newMethod(origin, access, name, desc, signature, exceptions)
         }
 
         override fun newField(origin: JvmDeclarationOrigin, access: Int, name: String, desc: String, signature: String?, value: Any?): FieldVisitor {
+            signatures += RawSignature(name, desc, MemberKind.FIELD) to origin.descriptor
             return super.newField(origin, access, name, desc, signature, value)
         }
 
         override fun done() {
+            if (firstClassWritten) outputStream.append(",\n") else firstClassWritten = true
+            outputStream.append("\t{\n")
+            origin.descriptor?.let {
+                outputStream.append("\t\t").appendNameValue("declaration", TYPE_RENDERER.render(it)).append(",\n")
+            }
+            outputStream.append("\t\t").appendNameValue("class", javaClassName ?: "<unknown>").append(", \n")
+
+            outputStream.append("\t\t").appendQuoted("members").append(": [\n")
+            signatures.joinTo(outputStream, ",\n") { buildString {
+                val (signature, descriptor) = it
+                append("\t\t\t{")
+                descriptor?.let {
+                    appendNameValue("declaration", MEMBER_RENDERER.render(it)).append(", ")
+                }
+                appendNameValue("name", signature.name).append(", ")
+                appendNameValue("desc", signature.desc).append("}")
+            }}
             outputStream.append("\n\t\t]\n\t}")
+
             super.done()
         }
     }
